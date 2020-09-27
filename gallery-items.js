e43e051d442d765c0f761d783b97ff2cf7b08dff
Alexander Tsotsko <63519514+RaiderTs@users.noreply.github.com>
2020-09-27 21:36:54 +0300

    добавлен prettierrc.json

--- gallery-items.js
+++ gallery-items.js
@@ -1,84 +1,84 @@
 const images = [
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
     original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
-    description: "Hokkaido Flower",
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
     original:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-    description: "Container Haulage Freight",
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
     original:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-    description: "Aerial Beach View",
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
     original:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-    description: "Flower Blooms",
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
     original:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-    description: "Alpine Mountains",
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
     original:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-    description: "Mountain Lake Sailing",
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
     original:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    description: "Alpine Spring Meadows",
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
     original:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    description: "Nature Landscape",
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
     original:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    description: "Lighthouse Coast Sea",
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
   },
 ];
 // Создаем refs
 const refs = {
-  galleryContainer: document.querySelector(".js-gallery"),
-  btnOpenModal: document.querySelector(".lightbox__button"),
-  lightboxEl: document.querySelector(".js-lightbox"),
-  lightboxImageEl: document.querySelector(".lightbox__image"),
+  galleryContainer: document.querySelector('.js-gallery'),
+  btnOpenModal: document.querySelector('.lightbox__button'),
+  lightboxEl: document.querySelector('.js-lightbox'),
+  lightboxImageEl: document.querySelector('.lightbox__image'),
   closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
-  backdropModal: document.querySelector(".lightbox__overlay"),
-  leftBtn: document.querySelector(".slider-arrow-left"),
-  rightBtn: document.querySelector(".slider-arrow-right"),
+  backdropModal: document.querySelector('.lightbox__overlay'),
+  leftBtn: document.querySelector('.slider-arrow-left'),
+  rightBtn: document.querySelector('.slider-arrow-right'),
 };
 
 //  Создаем разметку
 //////////////////////////////////////////////////////////////////
 const cardMarcap = createCardImagesMarcap(images);
-refs.galleryContainer.insertAdjacentHTML("beforeend", cardMarcap);
+refs.galleryContainer.insertAdjacentHTML('beforeend', cardMarcap);
 
 function createCardImagesMarcap(images) {
   return images
@@ -98,20 +98,20 @@ function createCardImagesMarcap(images) {
   </a>
 </li>`;
     })
-    .join("");
+    .join('');
 }
 // ставим слушателей
-refs.galleryContainer.addEventListener("click", onImageClick); //  открываем модалку по нажатию на картинку
-refs.closeModalBtn.addEventListener("click", onCloseModal);
-refs.backdropModal.addEventListener("click", onBackdropClick); //  закрываем модалку по кнопке
-refs.leftBtn.addEventListener("click", onBackSide); // листаем картинку влево
-refs.rightBtn.addEventListener("click", onNextSide); // листаем картинку вправо
+refs.galleryContainer.addEventListener('click', onImageClick); //  открываем модалку по нажатию на картинку
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.backdropModal.addEventListener('click', onBackdropClick); //  закрываем модалку по кнопке
+refs.leftBtn.addEventListener('click', onBackSide); // листаем картинку влево
+refs.rightBtn.addEventListener('click', onNextSide); // листаем картинку вправо
 
 //  Проверяем клик по IMG
 //  если равняется IMG тогда запускаем функция открытия модалки
 function onImageClick(evt) {
   event.preventDefault();
-  if (evt.target.nodeName !== "IMG") {
+  if (evt.target.nodeName !== 'IMG') {
     return;
   }
   onOpenModal(evt);
@@ -120,26 +120,26 @@ function onImageClick(evt) {
 }
 
 function onCloseModal(evt) {
-  window.removeEventListener("keydown", onEscPress);
-  refs.lightboxEl.classList.remove("is-open");
-  refs.lightboxImageEl.src = "";
-  refs.lightboxImageEl.alt = "";
+  window.removeEventListener('keydown', onEscPress);
+  refs.lightboxEl.classList.remove('is-open');
+  refs.lightboxImageEl.src = '';
+  refs.lightboxImageEl.alt = '';
 }
 
 // Функция для открытия модалки
 function onOpenModal(evt) {
-  window.addEventListener("keydown", onEscPress);
-  refs.lightboxEl.classList.add("is-open");
+  window.addEventListener('keydown', onEscPress);
+  refs.lightboxEl.classList.add('is-open');
 }
 
 //  Функция для закрытия модалки
 
 function onEscPress(evt) {
-  if (event.code === "Escape") {
+  if (event.code === 'Escape') {
     onCloseModal();
-  } else if (event.code === "ArrowLeft") {
+  } else if (event.code === 'ArrowLeft') {
     onBackSide();
-  } else if (event.code === "ArrowRight") {
+  } else if (event.code === 'ArrowRight') {
     onNextSide();
   }
 }
@@ -154,11 +154,11 @@ function onBackdropClick(evt) {
 
 // Ищем индекс
 function findIndex(evt) {
-  if (!refs.lightboxEl.classList.contains("is-open")) {
+  if (!refs.lightboxEl.classList.contains('is-open')) {
     return null;
   }
   return images.findIndex(
-    (element) => element.original === refs.lightboxImageEl.src
+    element => element.original === refs.lightboxImageEl.src,
   );
 }
 let imgIndex = findIndex(); // присваиваем переменную
